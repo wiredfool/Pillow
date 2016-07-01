55195584112f6158c21f94acde8fc8c11fcadb58
wiredfool <eric-github@soroos.net>
2016-07-01 02:23:52 -0700

    Another attempt at a crash

--- Tests/test_bmp_crop_crash.py
+++ Tests/test_bmp_crop_crash.py
@@ -8,12 +8,14 @@ base = os.path.join('Tests', 'images', 'bmp', 'g')
 class TestBmpCropCrash(PillowTestCase):
     def _testone(self,f):
         print('testing %s for crop crash'%f)
-        im = Image.open(os.path.join(base,f))
+        try:
+            im = Image.open(os.path.join(base,f))
+        except IOError: return
         im = im.crop((1,1,10,10))
         # should not crash
         im.load()
 
     def test_rgb(self):
-        for f in ['rgb16.bmp', 'rgb24.bmp', 'rgb32.bmp']:
+        for f in os.listdir(base):
             self._testone(f)
         
