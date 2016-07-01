dd8835974934ff3d3033742ce51b4b0fd8602322
wiredfool <eric-github@soroos.net>
2016-07-01 01:40:26 -0700

    test case for #1077?

---
+++ Tests/test_bmp_crop_crash.py
@@ -0,0 +1,19 @@
+from helper import unittest, PillowTestCase
+
+from PIL import Image
+import os
+
+base = os.path.join('Tests', 'images', 'bmp', 'g')
+
+class TestBmpCropCrash(PillowTestCase):
+    def _testone(self,f):
+        print('testing %s for crop crash'%f)
+        im = Image.open(os.path.join(base,f))
+        im2 = im.crop((1,1,10,10))
+        # should not crash
+        im2.load()
+
+    def test_rgb(self):
+        for f in ['rgb16.bmp', 'rgb24.bmp', 'rgb32.bmp']:
+            self._testone(f)
+        
